fa9380dc81179f25a7ebcf64c70b80e4c75880a2
Theodoros Matskas <theodoros.matskas@bnearit.se>
2018-08-24 13:58:19 +0200

    File init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyFirstAngularApp';
+  title = 'This is a changed title';
 }
