20eaa12ce90a04243e121feb4a3af5fc2af11b03
Theodoros Matskas <theodoros.matskas@bnearit.se>
2018-08-31 17:48:19 +0200

    added the observable

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from '../classes/hero';
 import { HEROES } from '../classes/mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 @Injectable({
@@ -9,9 +11,11 @@ import { HEROES } from '../classes/mock-heroes';
 
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
